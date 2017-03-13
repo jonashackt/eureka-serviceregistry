87068025db70ee54a6926a014d562d1a27c9b8f7
Jonas Hecht <jonas.hecht@codecentric.de>
2017-03-13 09:58:05 +0100

    Initial spring initializr commit

---
+++ src/main/java/de/jonashackt/EurekaServiceregistryApplication.java
@@ -0,0 +1,12 @@
+package de.jonashackt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceregistryApplication.class, args);
+	}
+}
