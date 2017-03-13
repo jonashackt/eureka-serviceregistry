a35ee98786a56a434d23b3f35dcb697d754052e5
Jonas Hecht <jonas.hecht@codecentric.de>
2017-03-13 10:23:30 +0100

    First working version

--- src/main/java/de/jonashackt/EurekaServiceregistryApplication.java
+++ src/main/java/de/jonashackt/EurekaServiceregistryApplication.java
@@ -2,8 +2,10 @@ package de.jonashackt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServiceregistryApplication {
 
 	public static void main(String[] args) {
